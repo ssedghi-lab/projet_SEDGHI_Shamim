b90a8c04762ec3b8159e2bc9c689578135d1cff0
shamim130 <shamim.sedghi@etu.unistra.fr>
2025-01-20 09:59:42 +0100

    change th environnement

--- FRONTEND/my-angular-app/src/app/environments/environment.ts
+++ FRONTEND/my-angular-app/src/app/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:3000/api",
-  backendCatalogue: "http://localhost:3000/api/products",
-  backendLoginClient: "http://localhost:3000/api/user/login",
-  backendAddClient: "http://localhost:3000/api/user/register",
-  backendUpdateClient: "http://localhost:3000/api/user/update-user",
-  backendGetClient: "http://localhost:3000/api/user/get-user",
+  apiUrl: "https://cnamapp-6goz.onrender.com/api",
+  backendCatalogue: "https://cnamapp-6goz.onrender.com/api/products",
+  backendLoginClient: "https://cnamapp-6goz.onrender.com/api/user/login",
+  backendAddClient: "https://cnamapp-6goz.onrender.com/api/user/register",
+  backendUpdateClient: "https://cnamapp-6goz.onrender.com/api/user/update-user",
+  backendGetClient: "https://cnamapp-6goz.onrender.com/api/user/get-user",
 };
